0598ee7b25b74a7ea63fc1b935c81e44805ba967
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-10-21 11:14:37 -0700

    Add routes and save data in memory

--- app.js
+++ app.js
@@ -10,11 +10,49 @@ const PORT = 3000;
 // Enable static file serving
 app.use(express.static('public'));
 
+// Allow the app to parse form data
+app.use(express.urlencoded({ extended: true}));
+
+// Create an array to store orders
+const orders = [];
+
 // Define a default "route" ('/')
 // req: contains information about the incoming request
 // res: allows us to send back a response to the client
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// Add a route for the form submission
+app.post('/submit-order', (req, res) => {
+    // Create a JSON object to store the data
+    const order = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        method: req.body.method,
+        toppings: req.body.toppings,
+        size: req.body.size
+    };
+    console.log(order);
+
+     // Add the order to our orders array
+    orders.push(order);
+    console.log(orders);
+
+    // Direct the user to the confirmation page
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// Define a "contact-us" route
+app.get('/contact-us', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+});
+
+// Define a "admin" route
+app.get('/admin', (req, res) => {
+    //res.sendFile(`${import.meta.dirname}/views/admin.html`);
+    res.send(orders);
 });
 
 // Start the server and listen on the specified port
